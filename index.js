da95c6231349f051348240c2ca8043b7d11de3a4
muhammadoginh <moginh@gmail.com>
2022-05-15 11:17:17 +0700

    add vapid key

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+
+const publicVapidKeys = 'BL0fGj1E6jenCMrGveDfuA-_xACla-rA5XKnb8xvaoCGYLyl03QX2Mrl5nr_mccMR1e3yj2kLDkeA-1GVMckEZE';
+const privateVapidKey = 'x6hP2-9gBb0jNTSGSY2iZK6AbyQ_y8MHraXEkw-pZBo';
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
-### how to start
+### how to start windows OS
 
 - npm init -y
 - npm i web-push express body-parser 
 - create index.js file
+- generate vapid keys, .\node_modules\.bin\web-push generate-vapid-keys
